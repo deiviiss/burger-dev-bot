ed4fae78b6621a604e6565ac0624d562aba0bdf7
David Hilera <david.hilera@hotmail.com>
2025-05-28 08:50:49 -0600

    feat: Add versioning to Baileys provider configuration

--- src/provider/index.ts
+++ src/provider/index.ts
@@ -1,4 +1,6 @@
 import { createProvider } from '@builderbot/bot'
 import { BaileysProvider as Provider } from '@builderbot/provider-baileys'
 
-export const baileysProvider = createProvider(Provider)
+export const baileysProvider = createProvider(Provider, {
+  version: [2, 3000, 1023223821]
+})
